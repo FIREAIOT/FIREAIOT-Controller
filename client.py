c7d96a9d61066c11151f990d25944894b3a4e0f8
Saleem Hadad <saleem_ha@hotmail.com>
2018-05-07 01:57:07 +0800

    update client.py

--- client.py
+++ client.py
@@ -1,28 +1,25 @@
+import sys
 import time
 import pysher
-import sys
-# Add a logging handler so we can see the raw communication data
 import logging
+
 root = logging.getLogger()
 root.setLevel(logging.INFO)
 ch = logging.StreamHandler(sys.stdout)
 root.addHandler(ch)
 
-pusher = pysher.Pusher("", cluster="ap1", secret="")
-
-def  my_func(*args, **kwargs):
+def  callbakc(*args, **kwargs):
     print("processing Args:", args)
     print("processing Kwargs:", kwargs)
 
-# We can't subscribe until we've connected, so we use a callback handler
-# to subscribe when able
 def connect_handler(data):
     channel = pusher.subscribe('alarms')
-    channel.bind('AlarmReceived', my_func)
+    channel.bind('AlarmReceived', callbakc)
+
+pusher = pysher.Pusher("", cluster="ap1", secret="")
 
 pusher.connection.bind('pusher:connection_established', connect_handler)
 pusher.connect()
 
 while True:
-    # Do other things in the meantime here...
     time.sleep(1)

3169119541e29a95b17f45bbb74dc15f353efaf6
Saleem Hadad <saleem_ha@hotmail.com>
2018-05-21 02:13:56 +0800

    update UAV class

--- FIREAIOT/UAV.py
+++ FIREAIOT/UAV.py
@@ -6,7 +6,7 @@ class UAV:
     def __init__(self, ip="tcp:127.0.0.1:5760"):
         self.vehicle = connect(ip, wait_ready=True)
 
-    def armAndTakeoff(self, altitude):
+    def armAndTakeOff(self, altitude):
         while not self.vehicle.is_armable:
             time.sleep(1)
 
@@ -22,3 +22,23 @@ class UAV:
             if self.vehicle.location.global_relative_frame.alt >= altitude * 0.95:
                 break
             time.sleep(1)
+
+    def getDistanceInMetres(location1, location2):
+        dlat = location2.lat - location1.lat
+        dlong = location2.lon - location1.lon
+        return math.sqrt((dlat*dlat) + (dlong*dlong)) * 1.113195e5
+
+
+    def goTo(latitdue, longitude):        
+        vehicle.airspeed = 3
+
+        point = LocationGlobalRelative(latitdue, longitude, 40)
+        vehicle.simple_goto(point)
+
+        while True:
+            if self.getDistanceInMetres(vehicle.location.global_frame, point) > 10:
+                break
+            time.sleep(1)
+
+
+
--- main.py
+++ main.py
@@ -7,20 +7,24 @@ from config.env import *
 from FIREAIOT.UAV import UAV
 from FIREAIOT.Client import Client
 
+print("Basic UAV's setup..")
+uav = UAV()
+
 def callback(data):
 	data    = json.loads(data)
 	action  = data["action"]
 	payload = data["payload"]
-
+	
 	if action == "isReady":
 		print("isReady command received..")
 	elif action == "goTo":
-		print("goTo command received with nodes..")
-		print(payload["nodes"])
-		
-
-# print("Basic UAV's setup..")
-# uav = UAV()
+		print("goTo command received with target..")
+		target    = payload["target"]
+		latitude  = target["latitude"]
+		longitude = target["longitude"]
+		print(latitude, longitude)
+		uav.armAndTakeOff(40)
+		uav.goTo(latitude, longitude)
 
 print("Crafting socket connection..")
 client = Client(
